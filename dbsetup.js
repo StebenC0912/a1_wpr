c0662e6bbca3e729607e03fc0a422cbf558e75d3
StebenC0912 <stebenc09122002@gmail.com>
2023-11-25 00:40:12 +0700

    update sign in function

--- dbsetup.js
+++ dbsetup.js
@@ -1,83 +1,74 @@
+const mysql = require('mysql2/promise');
+
 module.exports = async function () {
-  // Your database setup code here
-  const mysql = require("mysql2");
+  try {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "stebenc09122002",
+      database: "wpr2023",
+    });
 
-  const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "stebenc09122002",
-    database: "wpr2023",
-  });
-
-  connection.connect((err) => {
-    if (err) throw err;
     console.log("Connected to MySQL database");
 
     // create users table
     const createUsersTable = `
-    CREATE TABLE IF NOT EXISTS users (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL UNIQUE,
-      email VARCHAR(255) NOT NULL UNIQUE,
-      password VARCHAR(255) NOT NULL
-    )
-  `;
-    connection.query(createUsersTable, (err, result) => {
-      if (err) throw err;
-      console.log("Created users table");
-
-      // insert users
-      const insertUsers = `
-      INSERT IGNORE INTO users (name, email, password)
-      VALUES
-        ('User 1', 'a@a.com', 'a'),
-        ('User 2', 'b@b.com', 'a'),
-        ('User 3', 'c@c.com', 'a')
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+      )
     `;
-      connection.query(insertUsers, (err, result) => {
-        if (err) throw err;
-        console.log("Inserted users");
+    await connection.execute(createUsersTable);
+    console.log("Created users table");
 
-        // create emails table
-        const createEmailsTable = `
-        CREATE TABLE IF NOT EXISTS emails (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          sender_id INT NOT NULL,
-          recipient_id INT NOT NULL,
-          subject VARCHAR(255) NOT NULL,
-          body TEXT NOT NULL
-        )
-      `;
-        connection.query(createEmailsTable, (err, result) => {
-          if (err) throw err;
-          console.log("Created emails table");
+    // insert users
+    const insertUsers = `
+    INSERT IGNORE INTO users (username, email, password)
+    VALUES
+      ('User1', 'a@a.com', 'a'),
+      ('User2', 'b@b.com', 'a'),
+      ('User3', 'c@c.com', 'a')
+    `;
+    await connection.execute(insertUsers);
+    console.log("Inserted users");
 
-          // insert emails
-          const insertEmails = `
-          INSERT INTO emails (sender_id, recipient_id, subject, body)
-          VALUES
-            (1, 2, 'Subject 1', 'Body 1'),
-            (2, 1, 'Subject 2', 'Body 2'),
-            (1, 3, 'Subject 3', 'Body 3'),
-            (3, 1, 'Subject 4', 'Body 4'),
-            (2, 3, 'Subject 5', 'Body 5'),
-            (3, 2, 'Subject 6', 'Body 6'),
-            (1, 2, 'Subject 7', 'Body 7'),
-            (2, 1, 'Subject 8', 'Body 8')
-        `;
-          connection.query(insertEmails, (err, result) => {
-            if (err) throw err;
-            console.log("Inserted emails");
+    // create emails table
+    const createEmailsTable = `
+      CREATE TABLE IF NOT EXISTS emails (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        sender_id INT NOT NULL,
+        recipient_id INT NOT NULL,
+        subject VARCHAR(255) NOT NULL,
+        body TEXT NOT NULL
+      )
+    `;
+    await connection.execute(createEmailsTable);
+    console.log("Created emails table");
 
-            // close connection
-            connection.end((err) => {
-              if (err) throw err;
-              console.log("Closed MySQL database connection");
-            });
-          });
-        });
-      });
-    });
-  });
+    // insert emails
+    // insert emails
+        //   const insertEmails = `
+        //   INSERT INTO emails (sender_id, recipient_id, subject, body)
+        //   VALUES
+        //     (1, 2, 'Subject 1', 'Body 1'),
+        //     (2, 1, 'Subject 2', 'Body 2'),
+        //     (1, 3, 'Subject 3', 'Body 3'),
+        //     (3, 1, 'Subject 4', 'Body 4'),
+        //     (2, 3, 'Subject 5', 'Body 5'),
+        //     (3, 2, 'Subject 6', 'Body 6'),
+        //     (1, 2, 'Subject 7', 'Body 7'),
+        //     (2, 1, 'Subject 8', 'Body 8')
+            
+        // `;
+        //   await connection.query(insertEmails, (err, result) => {
+        //     if (err) throw err;
+        //     console.log("Inserted emails");
+        //   });
+
+  } catch (err) {
+    console.error('Error setting up database: ', err);
+  }
 };
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const db = require("mysql2");
+const ejs = require("ejs");
 const app = express();
 const port = 8000;
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+
 // set up database connection, run the dbsetup.js file to create the database and tables
 const dbSetup = require("./dbsetup");
+let query; 
 (async () => {
   try {
     await dbSetup();
@@ -22,70 +26,38 @@ const dbSetup = require("./dbsetup");
       database: "wpr2023",
     });
     connection.connect((err) => {
-      console.log("Connected to MySQL database");
+      console.log("Connected to MySQL database index");
     });
     // Serve the sign-in page
     app.get("/", (req, res) => {
-      res.send(`
-      <h1>Sign in</h1>
-      <form method="POST" action="/signin">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Sign in</button>
-      </form>
-      <p>Don't have an account? <a href="/signup">Sign up</a></p>
-    `);
-    });
-    // Handle sign-in form submission
-    app.post("/signin", (req, res) => {
-      const { username, password } = req.body;
-      // Check if username and password are valid
-      if (isValidCredentials(username, password)) {
-        // Create a session for the user and set a cookie with the session ID
-        const sessionId = createSession(username);
-        res.cookie("sessionId", sessionId);
+      if (req.cookies.sessionId) {
         res.redirect("/inbox");
       } else {
-        res.send(`
-      <h1>Sign in</h1>
-      <p>Invalid username or password</p>
-      <form method="POST" action="/signin">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Sign in</button>
-      </form>
-      <p>Don't have an account? <a href="/signup">Sign up</a></p>
-    `);
+        res.render("login", { error: req.query.error });
       }
     });
-    const response = `
-    <h1>Sign up</h1>
-    <form method="POST" action="/signup">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
-      <br>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-      <br>
-      <label for="re-enter-password">Re-enter password:</label>
-      <input type="password" id="re-enter-password" name="re-enter-password" required>
-      <br>
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
-      <br>
-      <button type="submit">Sign up</button>
-  `;
+    // Handle sign-in form submission
+    app.post("/", (req, res) => {
+      const { username, password } = req.body;
+      // Check if username and password are valid use mysql query
+      query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+      connection.query(query, (err, result) => {
+        if (err) throw err;
+        const isValid = result.length > 0;
+        if (isValid) {
+          // Create a session for the user and set a cookie with the session ID
+          const sessionId = createSession(username);
+          res.cookie("sessionId", sessionId);
+          res.redirect("/inbox");
+        } else {
+          const error = "Invalid information";
+          res.render("login", { error });
+        }
+      });
+    });
     // server get sign up page
     app.get("/signup", (req, res) => {
-      res.send(response);
+      res.render("signup", { error: req.query.error });
     });
     // Handle sign-up form submission
     app.post("/signup", (req, res) => {
@@ -97,9 +69,11 @@ const dbSetup = require("./dbsetup");
         email,
       } = req.body;
       // Check if username, password, and email are valid
-      if (password !== reEnterPassword) {
+      if (password < 6) {
+        notification = " The password is too short (less than 6 characters)"
+      } else if (password !== reEnterPassword) {
         notification =
-          "Password and re-enter password must be the same";
+          "Password and Re-enter password must be the same";
       } else if (isValidCredentials(username, password, email)) {
         notification = "Username or email already exists";
       } else {
@@ -108,11 +82,7 @@ const dbSetup = require("./dbsetup");
         res.cookie("sessionId", sessionId);
         res.redirect("/inbox");
       }
-      res.send(
-        response + `
-        <p id="notification">${notification}</p>
-      ` 
-      );
+      res.render("signup", { error: notification });
     });
     // Serve the inbox page
     app.get("/inbox", (req, res) => {
@@ -123,14 +93,14 @@ const dbSetup = require("./dbsetup");
       }
     });
 
-    // Helper functions
-    function isValidCredentials(username, password, email) {
-      // Check if username and password are valid
-      // Return true or false
-    }
+    
 
     function createSession(username) {
       // Create a session for the user and return the session ID
+      // use the cookie-session library to generate a session ID
+      // no need to store the session ID in the database
+      // just return it
+      return Math.floor(Math.random() * 1000000);
     }
 
     app.listen(port, () => {
