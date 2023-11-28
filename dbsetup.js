42b420e719ca34b6fb1fad1b6f7c43a18fb78b81
StebenC0912 <stebenc09122002@gmail.com>
2023-11-28 11:45:14 +0700

    update all methods

--- dbsetup.js
+++ dbsetup.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 
-module.exports = async function () {
+(async () => {
   try {
     const connection = await mysql.createConnection({
       host: "localhost",
@@ -23,14 +23,13 @@ module.exports = async function () {
     `;
     await connection.execute(createUsersTable);
     console.log("Created users table");
-
     // insert users
     const insertUsers = `
     INSERT IGNORE INTO users (email, password, fullName)
     VALUES
-      ('a@a.com', '123456', 'A'),
-      ('b@b.com', '123456', 'B'),
-      ('c@c.com', '123456', 'C')
+    ('a@a.com', '123456', 'Alice Doe'),
+    ('b@b.com', '123456', 'Bob Smith'),
+    ('c@c.com', '123456', 'Charlie Brown')
     `;
     await connection.execute(insertUsers);
     console.log("Inserted users");
@@ -44,6 +43,7 @@ module.exports = async function () {
         subject VARCHAR(255) NOT NULL,
         body TEXT NOT NULL,
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        externalFile VARCHAR(255),
         FOREIGN KEY (sender_id) REFERENCES users(id)
       )
     `;
@@ -51,21 +51,38 @@ module.exports = async function () {
     console.log("Created emails table");
 
     // insert emails
-    // insert emails
     const insertEmails = `
-          INSERT INTO emails (sender_id, recipient_id, subject, body, date)
-          VALUES
-            (1, 2, 'Subject 1', 'Body 1', '2021-02-01 00:00:00'),
-            (2, 1, 'Subject 2', 'Body 2', '2021-02-02 00:00:00'),
-            (1, 3, 'Subject 3', 'Body 3', '2021-02-03 00:00:00'),
-            (3, 1, 'Subject 4', 'Body 4', '2021-02-04 00:00:00'),
-            (2, 3, 'Subject 5', 'Body 5', '2021-02-05 00:00:00'),
-            (3, 2, 'Subject 6', 'Body 6', '2021-02-06 00:00:00'),
-            (1, 2, 'Subject 7', 'Body 7', '2021-02-07 00:00:00'),
-            (2, 1, 'Subject 8', 'Body 8', '2021-02-08 00:00:00')
-        `;
-    // await connection.query(insertEmails);
+  INSERT INTO emails (sender_id, recipient_id, subject, body, date)
+  VALUES
+    (1, 2, 'Hello', 'Hi there! How are you?', '2023-11-01 12:00:00'),
+    (2, 1, 'Meeting Tomorrow', 'Are you available for a meeting tomorrow?', '2023-11-02 14:30:00'),
+    (1, 3, 'Project Update', 'Here''s the latest update on the project.', '2023-11-03 09:45:00'),
+    (3, 1, 'Important Announcement', 'We have an important announcement to make.', '2023-11-04 16:20:00'),
+    (2, 3, 'Invitation', 'You''re invited to our event next week.', '2023-11-05 18:00:00'),
+    (3, 2, 'Feedback Needed', 'We would appreciate your feedback on the recent changes.', '2023-11-06 10:15:00'),
+    (1, 2, 'URGENT: Action Required', 'Action is required on the following matter.', '2023-11-07 13:30:00'),
+    (2, 1, 'Follow-up', 'Just following up on our previous conversation.', '2023-11-08 11:45:00'),
+    (1, 2, 'Special Announcement', 'We have a special announcement to share with you.', '2023-11-09 08:00:00'),
+    (2, 1, 'Exciting News', 'Exciting news! You won''t want to miss this.', '2023-11-10 15:00:00'),
+    (1, 3, 'Check this out', 'You should check this out.', '2023-11-11 10:30:00'),
+    (3, 1, 'Important Update', 'We have an important update regarding the project.', '2023-11-12 14:00:00'),
+    (2, 3, 'Meeting Minutes', 'Here are the meeting minutes from our last meeting.', '2023-11-13 16:45:00'),
+    (3, 2, 'Next Steps', 'Here are the next steps we need to take.', '2023-11-14 11:00:00'),
+    (1, 2, 'URGENT: Action Required', 'Action is required on the following matter.', '2023-11-15 09:30:00'),
+    (2, 1, 'Follow-up', 'Just following up on our previous conversation.', '2023-11-16 13:15:00'),
+    (1, 2, 'Special Announcement', 'We have a special announcement to share with you.', '2023-11-17 08:45:00'),
+    (2, 1, 'Exciting News', 'Exciting news! You won''t want to miss this.', '2023-11-18 14:30:00'),
+    (1, 3, 'Check this out', 'You should check this out.', '2023-11-19 16:00:00'),
+    (3, 1, 'Important Update', 'We have an important update regarding the project.', '2023-11-20 12:30:00'),
+    (2, 3, 'Meeting Minutes', 'Here are the meeting minutes from our last meeting.', '2023-11-21 09:00:00'),
+    (3, 2, 'Next Steps', 'Here are the next steps we need to take.', '2023-11-22 15:45:00'),
+    (1, 2, 'URGENT: Action Required', 'Action is required on the following matter.', '2023-11-23 11:30:00'),
+    (2, 1, 'Follow-up', 'Just following up on our previous conversation.', '2023-11-24 14:15:00')
+`;
+
+  
+    await connection.query(insertEmails);
   } catch (err) {
     console.error("Error setting up database: ", err);
   }
-};
+})();
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
 const db = require("mysql2");
 const ejs = require("ejs");
 const app = express();
@@ -9,16 +10,21 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
 
-// set up database connection, run the dbsetup.js file to create the database and tables
-const dbSetup = require("./dbsetup");
+const upload = multer({ storage: storage })
 let query;
 // Define the isValidCredentials function
 (async () => {
   try {
-    await dbSetup();
     // connect to the database
-
     const connection = db.createConnection({
       host: "localhost",
       port: 3306,
@@ -41,13 +47,11 @@ let query;
     // Handle sign-in form submission
     app.post("/", (req, res) => {
       const { email, password } = req.body;
-      console.log(email, password);
       // Check if username and password are valid use mysql query
       query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
       connection.query(query, (err, result) => {
         if (err) throw err;
         const isValid = result.length > 0;
-        console.log(result);
         if (isValid) {
           // Create a session for the user and set a cookie with the session ID
           res.cookie("sessionId", result[0].id);
@@ -94,21 +98,33 @@ let query;
         });
       }
     });
-    function getPaginatedEmails(userId, page, pageSize) {
-      // Your logic to retrieve paginated emails based on userId, page, and pageSize
-      // This is just a placeholder, replace it with your actual implementation
-      const emails = [
-        { id: 1, sender: "John Doe", subject: "Meeting Tomorrow" },
-        // Add more email objects as needed
-      ];
 
-      const startIdx = (page - 1) * pageSize;
-      const endIdx = startIdx + pageSize;
-      const paginatedEmails = emails.slice(startIdx, endIdx);
+    function getPaginatedEmails(page, userId, number) {
+      return new Promise((resolve, reject) => {
+        if (number === 1) {
+          query = `SELECT DISTINCT users.fullName AS senderName, emails.id, emails.subject, emails.date AS dateSent
+            FROM emails
+            JOIN users ON emails.sender_id = users.id
+            WHERE emails.recipient_id = ${userId}
+            ORDER BY emails.date DESC`;
+        } else {
+          query = `SELECT DISTINCT users.fullName AS senderName, emails.id, emails.subject, emails.date AS dateSent
+            FROM emails
+            JOIN users ON emails.recipient_id = users.id
+            WHERE emails.sender_id = ${userId}
+            ORDER BY emails.date DESC`;
+        }
+        connection.query(query, (err, result) => {
+          if (err) reject(err);
+          const emails = result;
 
-      const totalPages = Math.ceil(emails.length / pageSize);
-
-      return { emails: paginatedEmails, totalPages };
+          const startIdx = (page - 1) * 5;
+          const endIdx = startIdx + 5;
+          const paginatedEmails = emails.slice(startIdx, endIdx);
+          const totalPages = Math.ceil(emails.length / 5);
+          resolve({ emails: paginatedEmails, totalPages });
+        });
+      });
     }
 
     // Serve the inbox page
@@ -117,29 +133,198 @@ let query;
         // get the user's messages from the database
         // and pass them to the inbox page
         // page
-        query = "SELECT fullName FROM users WHERE id = ?";
-        let userInfoCurrent;
-        connection.query(query, [req.cookies.sessionId], (err, result) => {
-          if (err) throw err;
-          userInfoCurrent = result[0];
-          console.log(userInfoCurrent);
-        });
-        query = `SELECT users.fullName AS senderName, emails.subject, emails.date AS dateSent
-        FROM emails
-        JOIN users ON emails.sender_id = users.id;
-        `;
+        query = "SELECT * FROM users WHERE id = ?";
+        connection.query(
+          query,
+          [req.cookies.sessionId],
+          async (err, result) => {
+            if (err) throw err;
+            let fullName = result[0].fullName;
+            res.cookie("fullName", fullName);
+            let userId = req.cookies.sessionId;
+            const page = parseInt(req.query.page) || 1;
+            const { emails, totalPages } = await getPaginatedEmails(
+              page,
+              userId,
+              1
+            );
+            res.render("inbox", {
+              directory: "Inbox",
+              fullName: fullName,
+              emails: emails,
+              currentPage: page,
+              totalPages,
+              isOutbox: false,
+            });
+          }
+        );
+      } else {
+        res.redirect("/");
+      }
+    });
+    // server outbox
+    app.get("/outbox", (req, res) => {
+      if (req.cookies.sessionId) {
+        // get the user's messages from the database
+        // and pass them to the inbox page
+        // page
+        query = "SELECT * FROM users WHERE id = ?";
+        connection.query(
+          query,
+          [req.cookies.sessionId],
+          async (err, result) => {
+            if (err) throw err;
+            let fullName = result[0].fullName;
+            res.cookie("fullName", fullName);
+            let userId = req.cookies.sessionId;
+            const page = parseInt(req.query.page) || 1;
+            const { emails, totalPages } = await getPaginatedEmails(
+              page,
+              userId,
+              2
+            );
+            res.render("inbox", {
+              directory: "Outbox",
+              fullName: fullName,
+              emails: emails,
+              currentPage: page,
+              totalPages,
+              isOutbox: true,
+            });
+          }
+        );
+      } else {
+        res.redirect("/");
+      }
+    });
+    // server compose
+    app.get("/compose", (req, res) => {
+      if (req.cookies.sessionId) {
+        let fullName = req.cookies.fullName;
+        query = "SELECT * FROM users";
         connection.query(query, (err, result) => {
           if (err) throw err;
-          res.render("inbox", {
-            user: userInfoCurrent,
-            emails: result,
+          const users = result;
+          res.render("compose", {
+            fullName: fullName,
+            users: users,
+            error: "",
           });
         });
       } else {
         res.redirect("/");
       }
     });
+    // Handle compose form submission
+    app.post("/compose", upload.single("attachment"), (req, res) => {
+      let { recipient, subject, body } = req.body;
 
+      query = "SELECT * FROM users  WHERE email = ?";
+      let fullName = req.cookies.fullName;
+      console.log(body);
+      connection.query("SELECT * FROM users", (err, result) => {
+        if (err) throw err;
+        const usersList = result;
+        if (recipient === "") {
+          res.render("compose", {
+            fullName: fullName,
+            users: usersList,
+            error: "Please choose recipient",
+          });
+          return;
+        }
+        if (subject === "") {
+          subject = "No subject";
+        }
+        connection.query(query, [recipient], (err, result) => {
+          if (err) throw err;
+          if (result.length > 0) {
+            // Recipient found, you can access recipient details using result[0]
+            const recipientId = result[0].id;
+            if (recipientId == req.cookies.sessionId) {
+              res.render("compose", {
+                fullName: fullName,
+                users: usersList,
+                error: "You can't send email to yourself",
+              });
+            } else {
+
+              // Insert the email into the database
+              query =
+                "INSERT INTO emails (sender_id, recipient_id, subject, body, externalFile) VALUES (?, ?, ?, ?, ?)";
+              connection.query(query, [
+                req.cookies.sessionId,
+                recipientId,
+                subject,
+                body,
+                req.file ? req.file.originalname : null,
+              ]);
+              console.log("Email sent successfully");
+              res.render("compose", {
+                fullName: req.cookies.fullName,
+                users: usersList,
+                error: "Send success",
+              }); // Render the compose page with no error
+            }
+          } else {
+            // Recipient not found
+            console.log("Recipient does not exist");
+            res.render("compose", {
+              fullName: req.cookies.fullName,
+              users: usersList,
+              error: "Recipient does not exist",
+            });
+          }
+        });
+      });
+    });
+
+    // Serve the email detail page
+    app.get("/email/:id", (req, res) => {
+      if (req.cookies.sessionId) {
+        const emailId = req.params.id;
+        query = `SELECT sender.fullName AS senderName, receiver.fullName AS recipientName, emails.subject, emails.body, emails.date AS dateSent, emails.externalFile
+      FROM emails
+      JOIN users AS sender ON emails.sender_id = sender.id
+      JOIN users AS receiver ON emails.recipient_id = receiver.id
+      WHERE emails.id = ${emailId}`;
+      
+        connection.query(query, (err, result) => {
+          if (err) throw err;
+          let email = result[0];
+          console.log(email);
+          // replace \r\n with <br> tag
+          email.body = email.body.replace(/\r\n/g, "<br>");
+          res.render("email", {
+            email,
+            fullName: req.cookies.fullName,
+          });
+        });
+      } else {
+        res.redirect("/");
+      }
+    });
+    // download file
+    app.get("/uploads/:filename", (req, res) => {
+      const filename = req.params.filename;
+      res.download(`uploads/${filename}`, (err) => {
+        if (err) {
+          // Handle error, if any
+          console.error(err);
+          res.status(err.status).end();
+        } else {
+          // Redirect after the download is complete
+          res.on('finish', () => {
+            res.redirect("/inbox");
+          });
+        }
+      });
+    });
+    // sign out
+    app.get("/signout", (req, res) => {
+      res.clearCookie("sessionId");
+      res.redirect("/");
+    });
     app.listen(port, () => {
       console.log(`App listening at http://localhost:${port}`);
     });
